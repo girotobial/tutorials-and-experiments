5784133d57bb5af8ea7885d1d0652d57a2d9903e
opensomersault <abrobinson1907@gmail.com>
2020-03-27 18:06:25 +0000

    add chart styling

--- scipy-diff-solver.py
+++ scipy-diff-solver.py
@@ -2,7 +2,7 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-
+from seaborn import set_style
 # %%
 # Example 1: exponential decay
 def exponential_decay(t, y):
@@ -14,3 +14,8 @@ sol = solve_ivp(exponential_decay, [0, 10], [2, 4, 8])
 # %%
 for i in range(3):
     plt.plot(sol.t, sol.y[i])
+
+plt.xlabel(r'$t$')
+plt.ylabel(r'$y$')
+# %%
+# Example 2: Cannon fired upwards
